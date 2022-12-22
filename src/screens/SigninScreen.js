11ae3cb488b2f54c024de384c4dad62747de0986
ashrafstakala <ashrafstakala@gmail.com>
2022-12-22 22:22:32 +0200

    signup screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}> SigninScreen</Text>;
+  return <Text style={{ fontSize: 48, marginTop: 20 }}> SigninScreen</Text>;
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}> TrackCreateScreen</Text>;
+  return (
+    <Text style={{ fontSize: 48, marginTop: 20 }}> TrackCreateScreen</Text>
+  );
 };
 
 const styles = StyleSheet.create({});
