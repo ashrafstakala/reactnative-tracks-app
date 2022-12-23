6c32492fd18fbc88ee578ad0cd23acc3e4050255
ashrafstakala <ashrafstakala@gmail.com>
2022-12-23 17:59:46 +0200

    sign in screen with components

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48, marginTop: 20 }}> SigninScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign in to your account"
+        errorMessage=""
+        onSubmit=""
+        buttonText="Sign in"
+      />
+      <NavLink
+        text="Don't have an account? Sign up instead"
+        routeName="Signup"
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 150,
+  },
+});
 
 export default SigninScreen;
