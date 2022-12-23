b71c5f198cb77486576b0e488f4960276b434aa9
ashrafstakala <ashrafstakala@gmail.com>
2022-12-23 18:39:06 +0200

    clear error messages

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
-import Spacer from '../components/Spacer';
+import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         headerText="Sign up for Path"
         errorMessage={state.errorMessage}
