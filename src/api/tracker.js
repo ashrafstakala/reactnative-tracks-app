90fd89bd24b1d51ed808de6730756b0501984b80
ashrafstakala <ashrafstakala@gmail.com>
2022-12-26 17:42:31 +0200

    fetch track data from database

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
-  baseURL: 'https://f7d0-102-39-185-41.sa.ngrok.io',
+  baseURL: ' https://f062-102-39-185-41.eu.ngrok.io',
 });
 
 instance.interceptors.request.use(
