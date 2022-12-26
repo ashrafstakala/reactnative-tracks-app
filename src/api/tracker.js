94d370e066c17f16c46b547f63f3f26f8a0d2a8c
ashrafstakala <ashrafstakala@gmail.com>
2022-12-26 13:26:28 +0200

    automatic authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://f7d0-102-39-185-41.sa.ngrok.io',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
